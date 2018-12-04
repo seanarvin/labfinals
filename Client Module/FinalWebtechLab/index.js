b1f3d97f1474b2377292f4452e2938af7b408d62
seanarvin <2140366@slu.edu.ph>
2018-12-04 22:15:18 +0800

    added changes again

--- Client Module/FinalWebtechLab/index.js
+++ Client Module/FinalWebtechLab/index.js
@@ -1,5 +1,5 @@
-var express=require("express");
-var bodyParser=require('body-parser');
+var express = require("express");
+var bodyParser = require('body-parser');
  
 var connection = require('./config');
 var app = express();
