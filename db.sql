cf6c3f024339f1a5a3c415b6ed9676ad1fc3b6eb
Hizaya <hizayahizako@gmail.com>
2019-01-01 13:58:48 +0800

    ----------------

--- db.sql
+++ db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 29, 2018 at 02:36 AM
+-- Generation Time: Jan 01, 2019 at 06:58 AM
 -- Server version: 10.1.36-MariaDB
 -- PHP Version: 7.2.11
 
@@ -27,85 +27,113 @@ USE `db`;
 -- --------------------------------------------------------
 
 --
--- Table structure for table `facilities`
+-- Table structure for table `requests`
 --
 
-DROP TABLE IF EXISTS `facilities`;
-CREATE TABLE `facilities` (
-  `service_type` varchar(45) NOT NULL,
-  `rate` double DEFAULT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `facilities`
---
-
-INSERT INTO `facilities` (`service_type`, `rate`) VALUES
-('Plumber', 100),
-('Carpenter', 500);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `payment`
---
-
-DROP TABLE IF EXISTS `payment`;
-CREATE TABLE `payment` (
-  `pay_id` int(11) NOT NULL,
-  `pay_date` date DEFAULT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `payment`
---
-
-INSERT INTO `payment` (`pay_id`, `pay_date`) VALUES
-(101, '2018-11-26'),
-(102, '2018-11-28');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `service_provider`
---
-
-DROP TABLE IF EXISTS `service_provider`;
-CREATE TABLE `service_provider` (
+DROP TABLE IF EXISTS `requests`;
+CREATE TABLE `requests` (
+  `req_id` int(11) NOT NULL,
+  `date_requested` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `work_id` int(11) NOT NULL,
   `sp_id` int(11) NOT NULL,
-  `user_id` varchar(45) DEFAULT NULL,
-  `order_id` varchar(45) DEFAULT NULL,
-  `phone_no` int(11) DEFAULT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `service_provider`
---
-
-INSERT INTO `service_provider` (`sp_id`, `user_id`, `order_id`, `phone_no`) VALUES
-(11, '01', '1A', 912345698),
-(12, '02', '1B', 1234577921);
+  `date` date NOT NULL,
+  `from` time NOT NULL,
+  `to` time NOT NULL,
+  `note` text,
+  `client_id` int(11) NOT NULL,
+  `specifics_id` int(11) NOT NULL,
+  `status` enum('pending','rejected','ongoing','completed') NOT NULL DEFAULT 'pending'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `transaction`
+-- Table structure for table `services`
 --
 
-DROP TABLE IF EXISTS `transaction`;
-CREATE TABLE `transaction` (
-  `reservation_date` datetime NOT NULL,
-  `amount` double DEFAULT NULL,
-  `pay_id` varchar(45) DEFAULT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+DROP TABLE IF EXISTS `services`;
+CREATE TABLE `services` (
+  `service_id` int(11) NOT NULL,
+  `service_name` varchar(45) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `transaction`
+-- Dumping data for table `services`
 --
 
-INSERT INTO `transaction` (`reservation_date`, `amount`, `pay_id`) VALUES
-('2018-11-26 09:17:15', 100, '101'),
-('2018-11-28 15:17:12', 500, '102');
+INSERT INTO `services` (`service_id`, `service_name`) VALUES
+(1, 'Electrician '),
+(2, 'Plumbing'),
+(3, 'Carpentry'),
+(4, 'AC Repair');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `specifics`
+--
+
+DROP TABLE IF EXISTS `specifics`;
+CREATE TABLE `specifics` (
+  `specifics_id` int(11) NOT NULL,
+  `specifics` varchar(45) NOT NULL,
+  `service_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `spservice`
+--
+
+DROP TABLE IF EXISTS `spservice`;
+CREATE TABLE `spservice` (
+  `sp_id` int(11) NOT NULL,
+  `service_id` int(11) DEFAULT NULL,
+  `uid` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `spservices`
+--
+
+DROP TABLE IF EXISTS `spservices`;
+CREATE TABLE `spservices` (
+  `id` int(11) NOT NULL,
+  `category` varchar(20) NOT NULL,
+  `sp_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `spservices`
+--
+
+INSERT INTO `spservices` (`id`, `category`, `sp_id`) VALUES
+(1, '4', 8);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `spwork`
+--
+
+DROP TABLE IF EXISTS `spwork`;
+CREATE TABLE `spwork` (
+  `id` int(11) NOT NULL,
+  `work` varchar(20) NOT NULL,
+  `spservice_id` int(20) NOT NULL,
+  `price` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `spwork`
+--
+
+INSERT INTO `spwork` (`id`, `work`, `spservice_id`, `price`) VALUES
+(1, 'Not Cold/Not working', 1, 10),
+(2, 'asdasd', 2, 10);
 
 -- --------------------------------------------------------
 
@@ -116,59 +144,149 @@ INSERT INTO `transaction` (`reservation_date`, `amount`, `pay_id`) VALUES
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user` (
   `user_id` int(11) NOT NULL,
-  `password` varchar(45) DEFAULT NULL,
-  `user_fname` varchar(45) DEFAULT NULL,
-  `user_lname` varchar(45) DEFAULT NULL,
-  `contact_no` varchar(45) DEFAULT NULL,
-  `address` varchar(45) DEFAULT NULL,
-  `user_name` varchar(45) DEFAULT NULL,
+  `type` varchar(10) NOT NULL,
+  `user_fname` varchar(45) NOT NULL,
+  `user_lname` varchar(45) NOT NULL,
+  `user_name` varchar(45) NOT NULL,
+  `password` varchar(45) NOT NULL,
+  `contact_no` varchar(45) NOT NULL,
+  `address` varchar(45) NOT NULL,
   `email` varchar(45) DEFAULT NULL,
-  `status` varchar(20) NOT NULL,
-  `type` varchar(10) NOT NULL
+  `status` varchar(20) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`user_id`, `password`, `user_fname`, `user_lname`, `contact_no`, `address`, `user_name`, `email`, `status`, `type`) VALUES
-(1, '123', 'faye', 'lampa', '09123456789', 'Baguio City', 'fayelampa', 'fayelampa@yahoo.com', 'active', 'admin'),
-(2, '123', 'cam', 'poyaoan', '09123456798', 'Baguio City', 'camileru', 'camille@yahoo.com', 'disabled', 'sp'),
-(3, '123', 'admin', 'admin', '12314123', NULL, NULL, NULL, 'active', 'client');
+INSERT INTO `user` (`user_id`, `type`, `user_fname`, `user_lname`, `user_name`, `password`, `contact_no`, `address`, `email`, `status`) VALUES
+(8, 'sp', 'cam', 'cam', 'cam', '123', '09098080680', 'Dita lang', 'cam@gmail.com', 'disabled'),
+(9, 'admin', 'qwe', 'qwe', '123', '123', '12312', 'asda', 'asdasdasd@gmail.com', 'active'),
+(10, 'sp', 'a', 'b', 'q', 'q', '123123', 'asd', 'asdasdsfg@gmail.com', 'disabled');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `work`
+--
+
+DROP TABLE IF EXISTS `work`;
+CREATE TABLE `work` (
+  `work_id` int(11) NOT NULL,
+  `service_id` varchar(45) NOT NULL,
+  `description` varchar(45) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `work`
+--
+
+INSERT INTO `work` (`work_id`, `service_id`, `description`) VALUES
+(1, '4', 'Not Cold/Not working optimally');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `facilities`
+-- Indexes for table `requests`
 --
-ALTER TABLE `facilities`
-  ADD PRIMARY KEY (`service_type`);
+ALTER TABLE `requests`
+  ADD PRIMARY KEY (`req_id`);
 
 --
--- Indexes for table `payment`
+-- Indexes for table `services`
 --
-ALTER TABLE `payment`
-  ADD PRIMARY KEY (`pay_id`);
+ALTER TABLE `services`
+  ADD PRIMARY KEY (`service_id`);
 
 --
--- Indexes for table `service_provider`
+-- Indexes for table `specifics`
 --
-ALTER TABLE `service_provider`
+ALTER TABLE `specifics`
+  ADD PRIMARY KEY (`specifics_id`,`service_id`);
+
+--
+-- Indexes for table `spservice`
+--
+ALTER TABLE `spservice`
   ADD PRIMARY KEY (`sp_id`);
 
 --
--- Indexes for table `transaction`
+-- Indexes for table `spservices`
 --
-ALTER TABLE `transaction`
-  ADD PRIMARY KEY (`reservation_date`);
+ALTER TABLE `spservices`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `spwork`
+--
+ALTER TABLE `spwork`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `user`
 --
 ALTER TABLE `user`
   ADD PRIMARY KEY (`user_id`);
+
+--
+-- Indexes for table `work`
+--
+ALTER TABLE `work`
+  ADD PRIMARY KEY (`work_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `requests`
+--
+ALTER TABLE `requests`
+  MODIFY `req_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `services`
+--
+ALTER TABLE `services`
+  MODIFY `service_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `specifics`
+--
+ALTER TABLE `specifics`
+  MODIFY `specifics_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `spservice`
+--
+ALTER TABLE `spservice`
+  MODIFY `sp_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `spservices`
+--
+ALTER TABLE `spservices`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `spwork`
+--
+ALTER TABLE `spwork`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT for table `work`
+--
+ALTER TABLE `work`
+  MODIFY `work_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
