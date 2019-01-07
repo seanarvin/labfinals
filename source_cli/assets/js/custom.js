0d816144f7fd9e3e44cb115572ef26cd03846b4d
webdev337 <webdev337@gmail.com>
2019-01-07 11:54:48 +0800

    client bug fixes

--- source_cli/assets/js/custom.js
+++ source_cli/assets/js/custom.js
@@ -40,7 +40,7 @@ $(document).ready(function () {
                         <input id=specifics"${specifics.specifics_id}" value="${specifics.specifics_id}" type="radio" 
                         class="custom-control-input" name="specifics">
                         <label for=specifics"${specifics.specifics_id}"  class="custom-control-label">${specifics.specifics}</label>
-                        </div></div>`;
+                        </div>`;
 
                     });
 
@@ -67,7 +67,96 @@ $(document).ready(function () {
 
     $('#searchsp').on("click",function (event) {
         event.preventDefault();
-        let searchval = $('#searchinput').val();
+        search();
+    });
+
+    $('#searchinput').keypress(function(event){
+        event.preventDefault();
+        let keycode = (event.keyCode ? event.keyCode : event.which);
+        if(keycode == '13'){
+         search();
+        }
+    });
+
+    $('#passwordsubmit').on("click",function(event){
+        let oldpass = $('#oldpass');
+        let password = $('#password');
+        let confirm = $("#confirm");
+
+        let payload = {
+            "oldpass": oldpass.val() 
+        }
+        $.ajax({
+            url: "/validatepassword",
+            type: "POST",
+            data: JSON.stringify(payload),
+            contentType: "application/JSON",
+            success: function (result) {
+                if(result === true){
+                      oldpass.removeClass('is-invalid');
+                      oldpass.addClass('is-valid');
+                      $('#oldpasshelp').text("Password is correct.");
+                      if(password.val() === confirm.val() && password.val() !== ""){
+                          confirm.addClass('is-valid');
+                          confirm.removeClass('is-invalid');
+                            $('#confirmhelp').text("Password is the same.");
+                            alert("Password has been changed.");
+                          $('#changepassform').submit();
+                      }else{
+                        confirm.addClass('is-invalid');
+                        confirm.removeClass('is-valid');
+                        $('#confirmhelp').text("Password is not the same.");
+                      }
+                }else{
+                    oldpass.addClass('is-invalid');
+                    oldpass.removeClass('is-valid');
+                    $('#oldpasshelp').text("Password is not correct.");
+                }
+            }
+        });
+    });
+
+
+
+});
+
+
+//on submit form
+function submitRequest() {
+    let work = $('input[name=work]:checked').val();
+    let date = $('input[name=date]').val();
+    let from = $('input[name=from]').val();
+    let to = $('input[name=to]').val();
+    let specifics = $('input[name=specifics]:checked').val();
+    let sp = $('#nextBtn').data('uid');
+
+    if (!specifics) {
+        specifics = 0;
+    }
+
+    let payload = {
+        "work_id": work,
+        "date": date,
+        "from": from,
+        "to": to,
+        "specifics_id": specifics,
+        "sp_id": sp
+    }
+
+    $.ajax({
+        url: "/client/request",
+        type: "POST",
+        data: JSON.stringify(payload),
+        contentType: "application/JSON",
+        success: function (result) {
+            window.location.href = "/transactions";
+        }
+    });
+}
+
+//search
+function search(){
+    let searchval = $('#searchinput').val();
 
         window.location.href = "#spdiv";
 
@@ -112,43 +201,4 @@ $(document).ready(function () {
                 }
             });
         }
-    })
-
-
-});
-
-
-//on submit form
-function submitRequest() {
-    let work = $('input[name=work]').val();
-    let date = $('input[name=date]').val();
-    let from = $('input[name=from]').val();
-    let to = $('input[name=to]').val();
-    let specifics = $('input[name=specifics]').val();
-    let sp = $('#nextBtn').data('uid');
-
-    if (!specifics) {
-        specifics = 0;
-    }
-
-    let payload = {
-        "work_id": work,
-        "date": date,
-        "from": from,
-        "to": to,
-        "specifics_id": specifics,
-        "sp_id": sp
-    }
-
-    $.ajax({
-        url: "/client/request",
-        type: "POST",
-        data: JSON.stringify(payload),
-        contentType: "application/JSON",
-        success: function (result) {
-            window.location.href = "/transactions";
-        }
-    });
-
-
 }
